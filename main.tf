387861b4b865add505f8ae135e06784b255c6248
Jamie Caesar <jamie@thecaesars.us>
2020-05-20 17:41:02 -0400

    Adding US and EU VPCs

--- main.tf
+++ main.tf
@@ -18,6 +18,41 @@ locals {
   eu_az_suffixes = [for az in data.aws_availability_zones.eu_azs.names : trimprefix(az, "eu-central-1")]
   eu_azs         = slice(local.eu_az_suffixes, 0, var.num_azs > length(local.eu_az_suffixes) ? length(local.eu_az_suffixes) : var.num_azs)
 
-  common_tags = { "Owner" = "Jamie Caesar", "Company" = "Spacely Sprockets" }
+  common_tags = { "Owner" = "Jamie Caesar", "Company" = "Spacely Sprockets", "BU" = "Wordpress" }
 }
 
+# -----------------------------------------------------------------------------
+# US West 1 VPC
+# -----------------------------------------------------------------------------
+
+module "us_vpc" {
+  source  = "tfe.aws.shadowmonkey.com/spacelysprockets/ss_vpc/aws"
+  version = "0.0.6"
+
+  cidr_block = "10.13.0.0/16"
+  vpc_name   = "us_wordpress"
+  tags       = local.common_tags
+  azs        = local.us_azs
+
+  providers = {
+    aws = aws.us-west-1
+  }
+}
+
+# -----------------------------------------------------------------------------
+# EU Central 1 VPC
+# -----------------------------------------------------------------------------
+
+module "eu_vpc" {
+  source  = "tfe.aws.shadowmonkey.com/spacelysprockets/ss_vpc/aws"
+  version = "0.0.6"
+
+  cidr_block = "10.23.0.0/16"
+  vpc_name   = "eu_wordpress"
+  tags       = local.common_tags
+  azs        = local.eu_azs
+
+  providers = {
+    aws = aws.eu-central-1
+  }
+}
